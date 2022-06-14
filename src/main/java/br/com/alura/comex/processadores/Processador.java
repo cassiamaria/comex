a0a69212e81ffc16aa916f37dfb5741dad888b60
Cassia Maria <cassiamariaassis@gmail.com>
2022-06-14 15:04:40 -0300

    criação do projeto do zero

--- src/main/java/br/com/alura/comex/processadores/Processador.java
+++
@@ -1,9 +0,0 @@
-package br.com.alura.comex.processadores;
-
-import br.com.alura.comex.Pedido;
-
-import java.util.List;
-
-public interface Processador {
-    List<Pedido> processa() throws Exception;
-}
