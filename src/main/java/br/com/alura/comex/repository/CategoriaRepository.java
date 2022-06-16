662825762eece275ea20c018702e58c2a5f7e234
cassiamaria <cassiamariaassis@gmail.com>
2022-06-16 20:26:32 -0300

    pequeno ajuste

--- src/main/java/br/com/alura/comex/repository/CategoriaRepository.java
+++ src/main/java/br/com/alura/comex/repository/CategoriaRepository.java
@@ -4,5 +4,4 @@ import br.com.alura.comex.entity.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-    //Categoria findByNome(String nome);
 }
