255200a0efdb9856a8daa8ec0ec3d2cb68ffac32
cassiamaria <cassiamariaassis@gmail.com>
2022-07-04 15:14:43 -0300

    correção de bug na query

--- src/main/java/br/com/alura/comex/repository/PedidoRepository.java
+++ src/main/java/br/com/alura/comex/repository/PedidoRepository.java
@@ -8,12 +8,11 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface PedidoRepository  extends JpaRepository<Pedido, Long> {
-    @Query(value = "SELECT c.nome, COUNT(item.id) AS quantidadeProdutos, SUM((item.preco_unitario * item.quantidade)) AS montanteVendido " +
-            "FROM pedidos " +
-            "JOIN itens_pedido item " +
+    @Query(value = "SELECT c.nome, SUM(item.quantidade) AS quantidadeProdutos, SUM((item.preco_unitario * item.quantidade)) AS montanteVendido " +
+            "FROM itens_pedido item " +
             "JOIN produtos p " +
             "JOIN categorias c " +
-            "WHERE pedidos.id = item.pedido_id AND item.produto_id = p.id AND p.categoria_id = c.id " +
-            "GROUP BY c.id, pedidos.id, item.id", nativeQuery = true)
+            "WHERE item.produto_id = p.id AND p.categoria_id = c.id " +
+            "GROUP BY item.produto_id, c.nome", nativeQuery = true)
     List<PedidosPorCategoriaProjecao> findPedidosPorCategoria();
 }
